10b31aa2876460344a3111dbd7931741dd584938
zhanghaoran12 <15295180301@163.com>
2019-12-25 21:56:46 +0800

    add bootstrap

--- src/main/java/com/majiang/community/demo/controller/IndexController.java
+++ src/main/java/com/majiang/community/demo/controller/IndexController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-
+public class IndexController {
+    /*
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
+    }
+     */
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
